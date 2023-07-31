1089791f6f88ce1a1648a9cfbf044caa4c7f9fe2
Roman <jrj80mail@gmail.com>
2023-07-31 23:26:35 +0300

    import SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,38 @@
-// Add imports above this line
+//* Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+//* Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+//! Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-// Hello world
+//! Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+gallerySetToHtml(createGalleryMarkup(galleryItems));
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, preview, original }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+       </li>`
+    )
+    .join('');
+}
+
+function gallerySetToHtml(gallery) {
+  galleryList.innerHTML = gallery;
+}
 
 console.log(galleryItems);
